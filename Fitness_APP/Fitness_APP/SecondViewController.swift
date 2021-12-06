6d52311da44af7292a8c829c9136a35f31e72e70
Kfir Flank <kfirflank@Kfirs-MacBook-Pro.local>
2021-12-05 23:36:50 -0500

    buttons and drop down

---
+++ Fitness_APP/Fitness_APP/SecondViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SecondViewController.swift
+//  Fitness_APP
+//
+//  Created by Kfir Flank on 12/5/21.
+//
+import DropDown
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    let menu: DropDown = {
+        let menu = DropDown()
+        menu.dataSource = [
+            "drop_down1",
+            "drop_down2",
+            "drop_down3",
+            "drop_down4",
+            "drop_down5",
+            "drop_down6"
+        ]
+        return menu
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let myView = UIView(frame: navigationController?.navigationBar.frame ?? .zero)
+        myView.backgroundColor = .blue
+        navigationController?.navigationBar.topItem?.titleView = myView
+        guard let topView = navigationController?.navigationBar.topItem?.titleView else {
+            return}
+        menu.anchorView = topView
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapTopItem))
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        topView.addGestureRecognizer(gesture)
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    @objc func didTapTopItem() {
+        menu.show()
+    }
+    @IBOutlet weak var Label2_Controller2: UILabel!
+    @IBOutlet weak var Text_field_controller2: UITextField!
+    @IBAction func Secondcontroller(_ sender: Any) {
+        if let name = Text_field_controller2.text {
+            Label2_Controller2.text! += Text_field_controller2.text! + "\n"
+            Label2_Controller2.numberOfLines = 0
+        }
+    }
+    
+    
+    
+    
+    
+}
