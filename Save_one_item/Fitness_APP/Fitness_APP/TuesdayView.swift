1ddd85467f8b88fc8c8175718deecf90633f4e87
Kfir Flank <kfirflank@dhcp-wifi-8021x-168-122-203-75.bu.edu>
2021-12-08 00:51:54 -0500

    This is the first trial for saving data

---
+++ Save_one_item/Fitness_APP/Fitness_APP/TuesdayView.swift
@@ -0,0 +1,22 @@
+//
+//  TuesdayView.swift
+//  Fitness_APP
+//
+//  Created by Kfir Flank on 12/6/21.
+//
+
+import UIKit
+
+
+class TuesdayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+    
+
+
+
